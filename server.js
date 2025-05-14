758232e1545fac2483c8e95646276e7922177139
Pranav Pathak <37581918+PranavPathak10@users.noreply.github.com>
2025-05-14 17:59:55 +0530

    Update server.js finally

--- server.js
+++ server.js
@@ -11,9 +11,10 @@ app.use(bodyParser.json());
 app.use(express.static('public'));
 
 // Connect to MongoDB
-// Connect to MongoDB
-mongoose.connect('mongodb+srv://pathakpranav93:happy123@cluster-saarthi.813ngbj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster-Saarthi')
-  .then(() => console.log('Connected to MongoDB'))
+mongoose.connect('mongodb+srv://pathakpranav93:pranav123@cluster-saarthi.813ngbj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster-Saarthi', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).then(() => console.log('Connected to MongoDB'))
   .catch(err => console.error(err));
 
 
